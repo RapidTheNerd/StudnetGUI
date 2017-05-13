426ea269b7fc5d06c6d9725a3e50e106300debce
RapidTheNerd <george.marr.cobc@gmail.com>
2017-05-13 14:39:22 +0100

    database work

--- src/sample/Database.java
+++ src/sample/Database.java
@@ -1,22 +1,4 @@
-package sample;/*
- * StudentGUI
- * Copyright (C) 2017 RapidTheNerd
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-import java.util.ArrayList;
+package sample;
 import java.util.HashMap;
 
 public class Database {
@@ -24,5 +6,14 @@ public class Database {
      * To make the code more efficient and clean we can use multiple classes to store and maintain systems such as this.
      * It allows us later on the add and change values in the class without having to go through one class digging for it
      */
-    private static HashMap<Integer, ArrayList<String>> students = new HashMap<Integer, ArrayList<String>>();
+    private static HashMap<Integer, String> students = new HashMap<Integer, String>();
+
+    public static void addToList(int id, String course){
+        students.put(id, course);
+    }
+    public static void checkIfIsInAlready(int id, String course){
+        if(students.containsKey(id) && students.containsKey(course)){
+
+        }
+    }
 }
