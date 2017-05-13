847962e7aa80a981e6ae42c7e42dfc116d0bff7c
RapidTheNerd <george.marr.cobc@gmail.com>
2017-05-13 14:49:48 +0100

    working on the main window

--- src/sample/Database.java
+++ src/sample/Database.java
@@ -8,13 +8,22 @@ public class Database {
      */
     private static HashMap<Integer, String> students = new HashMap<Integer, String>();
 
-    public static void addToList(int id, String course){
+    public static void addToList(int id, String course){ //adding to the list
         students.put(id, course);
     }
     public static void checkIfIsInAlready(int id, String course){
         if(students.containsKey(id) && students.containsKey(course)){
             ErrorWindow.createErrorWindow("Error: Student is already in the database");
             //Here is where we allow the usage of the method below
+        }
+    }
+    public static void removeFromList(int id, String course){ //removing from the list if you need it
+        if (students.containsKey(id) && students.containsKey(course)) { // this checks if they're id and course is in the
+            //database, we can't check both values at the same time so we have to use &&
+            students.remove(id);
+            students.remove(course);
+        } else {
+            ErrorWindow.createErrorWindow("Error: That student is not in the database");
         }
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,8 +1,16 @@
 package sample;
 
-public class Main {
+import javax.swing.*;
+
+public class Main extends JFrame{
 
     public static void main(String[] args) {
+        //no need for a seperate class for the main window
+        JFrame mainWindow = new JFrame();
 
+        mainWindow.setVisible(true);
+        mainWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        mainWindow.setLocationRelativeTo(null); //setting this will make sure that the window is always on the centre of the screen
+        mainWindow.setSize(250, 250); //needs adjusting
     }
 }
