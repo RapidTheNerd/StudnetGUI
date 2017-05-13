d1e209961d90de263c1928dcb5e70819a6840378
RapidTheNerd <george.marr.cobc@gmail.com>
2017-05-13 15:04:31 +0100

    student course input

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,6 +12,7 @@ public class Main extends JFrame{
         JPanel mainPanel = new JPanel();
         final JTextField studentID = new JTextField(); //text fields are a bit of a pain in the ass to get int values out of
         //but there's a way we can do that
+        final JTextField studentCourse = new JTextField();
         JButton addButton = new JButton();
 
 
@@ -25,7 +26,12 @@ public class Main extends JFrame{
 
         studentID.setColumns(10); // for some reason in Java the size of the textField is set in columns, not using setSize
         studentID.setSize(50, 50);
-        studentID.setToolTipText("Student ID"); //we can add some text to signify what it is
+        studentID.setText("Student ID"); //we can add some text to signify what it is
+
+        //same as above
+        studentCourse.setText("Student course");
+        studentCourse.setColumns(10);
+        studentCourse.setSize(50, 50);
 
         mainWindow.setVisible(true);
         mainWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -36,11 +42,12 @@ public class Main extends JFrame{
             @Override
             public void actionPerformed(ActionEvent e) {
                 int idInput = Integer.parseInt(studentID.getText());//parsing it to an integer allows us to check if it's > 0
+                String courseInput = studentCourse.getText().toUpperCase(); //calling to upper case will mean that it doesn't care about capitals
                 if(idInput < 0){
                     System.out.println("ID is less than 0");
                     ErrorWindow.createErrorWindow("Error: ID can't be less than 0");
                 } else {
-
+                    Database.addToList(idInput, courseInput);
                 }
             }
         });
