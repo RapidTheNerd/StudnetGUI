74bf9fe4028b96451394ca8a4bfb47534ab7b4e2
RapidTheNerd <george.marr.cobc@gmail.com>
2017-05-13 14:57:31 +0100

    adding  the action listener

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,6 +1,8 @@
 package sample;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main extends JFrame{
 
@@ -8,7 +10,7 @@ public class Main extends JFrame{
         //no need for a seperate class for the main window
         JFrame mainWindow = new JFrame();
         JPanel mainPanel = new JPanel();
-        JTextField studentID = new JTextField(); //text fields are a bit of a pain in the ass to get int values out of
+        final JTextField studentID = new JTextField(); //text fields are a bit of a pain in the ass to get int values out of
         //but there's a way we can do that
 
         mainPanel.add(studentID);
@@ -17,10 +19,21 @@ public class Main extends JFrame{
         studentID.setColumns(10); // for some reason in Java the size of the textField is set in columns, not using setSize
         studentID.setSize(50, 50);
         studentID.setToolTipText("Student ID"); //we can add some text to signify what it is
-        
+
         mainWindow.setVisible(true);
         mainWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         mainWindow.setLocationRelativeTo(null); //setting this will make sure that the window is always on the centre of the screen
         mainWindow.setSize(250, 250); //needs adjusting
+
+        //here we're going to listen out for the users input and check if it's correct for the ID
+        studentID.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int idInput = Integer.parseInt(studentID.getText()); //parsing it to an integer allows us to check if it's > 0
+                if(idInput > 0){
+                    ErrorWindow.createErrorWindow("Error: ID can't be less than 0");
+                }
+            }
+        });
     }
 }
